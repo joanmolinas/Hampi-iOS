780af661e2e977b0dc9d1fe73315e9a3ee5fa302
Joan Molinas Ramon <joanmramon@gmail.com>
2017-05-25 00:29:44 +0200

    HMPFirebase framework created

---
+++ Hampi/HMPFirebase/HMPFirebase.h
@@ -0,0 +1,19 @@
+//
+//  HMPFirebase.h
+//  HMPFirebase
+//
+//  Created by Joan Molinas Ramon on 25/5/17.
+//  Copyright © 2017 com.hamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HMPFirebase.
+FOUNDATION_EXPORT double HMPFirebaseVersionNumber;
+
+//! Project version string for HMPFirebase.
+FOUNDATION_EXPORT const unsigned char HMPFirebaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMPFirebase/PublicHeader.h>
+
+
